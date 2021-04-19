6f64f6c15aeaa8e0fe02b8e606cd442b4ad9b297
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-19 15:58:45 +0530

    CountInSubSets Problem.

--- src/Practise.java
+++ src/Practise.java
@@ -1,10 +1,15 @@
 public class Practise {
 
     public static void main(String[] args) {
-        int n = 5;
-        System.out.println(Integer.toBinaryString(n));
-        System.out.println(Integer.toHexString(n));
-        System.out.println(Integer.toOctalString(n));
+       int[] a = {10,15,25};
+
+        System.out.println(countInSubSets(a,25,a.length));
     }
 
+    public static int countInSubSets(int[] arr,int sum,int n){
+        if(n == 0)
+            return (sum == 0)?1:0;
+
+        return countInSubSets(arr,sum,n-1) + countInSubSets(arr,sum - arr[n-1],n-1);
+    }
 }
