3b46fa51aa9ea13d7469e8df7ca6a3117b353456
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-19 15:54:17 +0530

    Worked on Bit in Java!

---
+++ src/Practise.java
@@ -0,0 +1,10 @@
+public class Practise {
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(Integer.toHexString(n));
+        System.out.println(Integer.toOctalString(n));
+    }
+
+}
