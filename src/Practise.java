1dedfab2a27a20d32ee1a10ff90f79eda4f6f203
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-21 20:15:47 +0530

    Commit

--- src/Practise.java
+++ src/Practise.java
@@ -1,10 +1,27 @@
 public class Practise {
 
     public static void main(String[] args) {
-       int[] a = {10,15,25};
+       int[] a = {1,2,3,4,5,6,7,8};
 
-        System.out.println(countInSubSets(a,25,a.length));
+        leftRotate(a,3,a.length);
     }
+
+    public static void leftRotate(int[] arr,int d,int n){
+    		reverseArray(arr,0,d-1);
+    		reverseArray(arr,d,n-1);
+    		reverseArray(arr,0,n-1);
+    		for (int i=0;i<n ;i++ ) {
+    			System.out.println(arr[i]);
+    		}
+    }
+
+	public static void reverseArray(int[] arr,int low,int high){
+		while(low<high){
+			swap(arr,low,high);
+			low++;
+			high--;
+		}
+	}
 
     public static int countInSubSets(int[] arr,int sum,int n){
         if(n == 0)
@@ -13,7 +30,7 @@ public class Practise {
         return countInSubSets(arr,sum,n-1) + countInSubSets(arr,sum - arr[n-1],n-1);
     }
 
-    public static void reverseArray(int[] arr){
+    public static void reverseArrayFull(int[] arr){
 		int low = 0,high = arr.length;
 
 		while(low<high){
@@ -27,8 +44,6 @@ public class Practise {
 		arr[i]  =  arr[j];
 		arr[j]  =  temp;
 	}
-
-
 	public static void removeDuplicates(int[] arr){
 		int n= arr.length;
 		int res = 1;
