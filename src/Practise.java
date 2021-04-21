83984b2d681d02434a09667d52f9f5c95821fb30
Bharath Kalyan S <sbharath35@gmail.com>
2021-04-21 20:06:11 +0530

    Commit

--- src/Practise.java
+++ src/Practise.java
@@ -12,4 +12,35 @@ public class Practise {
 
         return countInSubSets(arr,sum,n-1) + countInSubSets(arr,sum - arr[n-1],n-1);
     }
+
+    public static void reverseArray(int[] arr){
+		int low = 0,high = arr.length;
+
+		while(low<high){
+			swap(arr,low,high);
+			low++;
+			high--;
+		}
+	}
+	public static void swap(int [] arr,int i,int j){
+		int temp = arr[i];
+		arr[i]  =  arr[j];
+		arr[j]  =  temp;
+	}
+
+
+	public static void removeDuplicates(int[] arr){
+		int n= arr.length;
+		int res = 1;
+		for (int i=1;i<n ;i++ ) {
+			if (arr[i-1]!= arr[i]) {
+				arr[res] = arr[i];
+				res++;
+			}
+		}
+
+		for (int i=0 ;i<res ;i++ ) {
+			System.out.println(arr[i]);
+		}
+	}
 }
