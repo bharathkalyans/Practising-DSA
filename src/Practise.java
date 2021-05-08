ae68bc57a9cd7ca985b032f013652ce4a957a5aa
Bharath Kalyan S <sbharath35@gmail.com>
2021-05-08 23:03:42 +0530

    Valid Palindrome with 1 letter being Exempted

--- src/Practise.java
+++ src/Practise.java
@@ -5,11 +5,34 @@ public class Practise {
 
     public static void main(String[] args) {
 
-    	int[] a= new int[]{3,9,0,12,-1,1000,2,1,4,5};
-
 
 
     }
+
+	public boolean validPalindrome(String s) {
+		int left = 0;
+		int right = s.length()-1;
+
+		while(left < right) {
+			if(s.charAt(left) != s.charAt(right)) {
+				return checkPalindrome(s, left, right-1) || checkPalindrome(s, left+1, right);
+			}
+			left++;
+			right--;
+		}
+		return true;
+	}
+
+	private boolean checkPalindrome(String s, int left, int right) {
+		while(left < right) {
+			if(s.charAt(left) != s.charAt(right)) {
+				return false;
+			}
+			left++;
+			right--;
+		}
+		return true;
+	}
 
     public static void QuickSort(int[] arr,int l,int h){
 
