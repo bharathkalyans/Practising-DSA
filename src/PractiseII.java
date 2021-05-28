6e8fdf6b8c4c18b93a12f8b98d2fb15908738c57
Bharath Kalyan S <sbharath35@gmail.com>
2021-05-28 11:54:17 +0530

    Reversing a Linked List!

--- src/PractiseII.java
+++ src/PractiseII.java
@@ -12,11 +12,45 @@ public class PractiseII {
         head.next.next.next = new Node(4);
         head.next.next.next.next = new Node(5);
 
-        printList(head);
-        System.out.println(MiddleOfLinkedList(head).key);
+        Node res= ReverseLinkedList(head);
+        printList(res);
+        System.out.println("Size is :: " + sizeOfLinkedList(res));
 
     }
 
+
+    public static Node ReverseLinkedList(Node node){
+
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+
+    }
+
+    public static int sizeOfLinkedList(Node head){
+        if (head == null)
+            return 0;
+        if (head.next == null)
+            return 1;
+
+
+        int count = 0;
+
+        while (head!=null) {
+            count++;
+            head = head.next;
+        }
+
+        return count;
+    }
 
     public static Node MiddleOfLinkedList(Node head){
 
