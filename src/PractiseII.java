9b875798b5a11dc995bda3d01c13da45ae8025b5
Bharath Kalyan S <sbharath35@gmail.com>
2021-05-29 15:08:42 +0530

    Detecting a Cycle/Loop in a Linked List!Used both Floyd and Richard Brent Algorithm.

--- src/PractiseII.java
+++ src/PractiseII.java
@@ -15,13 +15,51 @@ public class PractiseII {
         head.next.next.next.next.next = new Node(6);
 
 
-        printList(head);
-        Node res = ReverseLinkedListByKUsingStack(head, 2);
-        printList(res);
-
+        System.out.println(hasLoop(head));
+        System.out.println(hasLoopFloydCycleDetection(head));
 
     }
 
+    public static boolean hasLoopFloydCycleDetection(Node head){
+        if (head == null || head.next == null)
+            return false;
+
+        Node fast = head,slow = head;
+
+        while (fast!=null && fast.next!=null){
+
+            slow  = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+
+    }
+
+    public static boolean hasLoop(Node root) {
+        //Richard Brent Algorithm : :
+        //He claims that this algorithm is 20 -34 % faster than Floyd Cycle detection Algorithm.
+        if (root == null) return false;
+
+        Node slow = root, fast = root;
+        int taken = 0, limit = 2;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            taken++;
+            if (slow == fast) return true;
+
+            if (taken == limit) {
+                taken = 0;
+                limit <<= 1;    // equivalent to limit *= 2;
+                slow = fast;    // teleporting the turtle (to the hare's position)
+            }
+        }
+        return false;
+    }
 
     //Recursive Solution
     public static Node ReverseLinkedListByK(Node head, int k) {
