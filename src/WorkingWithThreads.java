64285260fdd19cda3c6587b76b70f7c5e2d52258
Bharath Kalyan S <sbharath35@gmail.com>
2021-06-30 20:06:45 +0530

    Printing Odd Even using two threads!

--- src/WorkingWithThreads.java
+++ src/WorkingWithThreads.java
@@ -1,20 +1,68 @@
 
 
 public class WorkingWithThreads {
+
+    int counter = 1;
+    private static int N;
+
+    public void printOddNumber() {
+        synchronized (this) {
+            while (counter < N) {
+                while (counter % 2 == 1) {
+                    try {
+                        wait();
+                    } catch (
+                            InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                System.out.print(counter + " ");
+                counter++;
+
+                notify();
+            }
+        }
+    }
+
+    public void printEvenNumber() {
+        synchronized (this) {
+            while (counter < N) {
+                while (counter % 2 == 0) {
+                    try {
+                        wait();
+                    } catch (
+                            InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.print(counter + " ");
+                counter++;
+
+                notify();
+            }
+        }
+    }
+
     public static void main(String[] args) {
+
+        WorkingWithThreads object = new WorkingWithThreads();
+        N = 50;
+
+
 
         Thread Thread1 = new Thread() {
             public void run() {
-                System.out.println("Hello I am in a Thread" + this.getName());
-
+                object.printEvenNumber();
             }
         };
 
         Thread Thread2 = new Thread() {
             public void run() {
-                System.out.println("Hello I am in a Thread" + this.getName());
+                object.printOddNumber();
             }
         };
+
 
         Thread1.start();
         Thread2.start();
