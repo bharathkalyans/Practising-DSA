4830ea2cbb1a7567247e8460abd410235a37481f
Bharath Kalyan S <sbharath35@gmail.com>
2021-09-03 19:08:12 +0530

    Knights Tour Problem!!

--- src/PractiseVIII.java
+++ src/PractiseVIII.java
@@ -4,15 +4,69 @@ public class PractiseVIII {
     //Mostly BackTracking!
     public static void main(String[] args) {
 
-        int maze[][] = {
-                {1, 0, 1, 1, 1},
-                {1, 1, 1, 0, 1},
-                {0, 0, 0, 0, 1},
-                {0, 0, 0, 1, 1},
-                {0, 0, 0, 0, 1}};
+        KnightTourProblem();
 
-        RatInMaze(maze);
+    }
 
+    public static void KnightTourProblem() {
+        int N = 8;
+        int[][] solution = new int[8][8];
+
+        for (int x = 0; x < N; x++)
+            for (int y = 0; y < N; y++)
+                solution[x][y] = -1;
+
+
+        solution[0][0] = 0;
+
+        //Knight Moves!!
+        int[] xMove = {2, 1, -1, -2, -2, -1, 1, 2};
+        int[] yMove = {1, 2, 2, 1, -1, -2, -2, -1};
+
+
+        if (!KnightTourProblemSolver(solution, 0, 0, N, 1, xMove, yMove)) {
+            System.out.println("Not Possible");
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    System.out.print(solution[i][j] + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            //Printing the Result!!
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    System.out.print(solution[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    public static boolean isSafe(int x, int y, int sol[][], int N) {
+        return (x >= 0 && x < N && y >= 0 && y < N && sol[x][y] == -1);
+    }
+
+    private static boolean KnightTourProblemSolver(int[][] solution, int i, int j, int n, int index, int[] xMove, int[] yMove) {
+
+        int next_x_move, next_y_move;
+
+        int square = n * n;
+        if (index == square)
+            return true;
+        //You could have used 8 if statements! Copied this method from GFG!!🥸
+        for (int k = 0; k < 8; k++) {
+            next_x_move = i + xMove[k];
+            next_y_move = j + yMove[k];
+            if (isSafe(next_x_move, next_y_move, solution, n)) {
+                solution[next_x_move][next_y_move] = index;
+                if (KnightTourProblemSolver(solution, next_x_move, next_y_move, n, index + 1, xMove, yMove))
+                    return true;
+                else solution[next_x_move][next_y_move] = -1; // Backtracking Important Step!
+            }
+
+        }
+        return false;
     }
 
     public static void RatInMaze(int[][] Maze) {
