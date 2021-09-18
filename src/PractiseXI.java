8b4749808f135a2108ca6046ab4fe24fe3f42919
Bharath Kalyan S <sbharath35@gmail.com>
2021-09-18 11:11:31 +0530

    PrintLongestCommonSubSequence using LCS!! DP Array!

--- src/PractiseXI.java
+++ src/PractiseXI.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PractiseXI {
 
@@ -7,8 +9,8 @@ public class PractiseXI {
     //Mostly DP Problems!
     public static void main(String[] args) {
 
-        String A = "OldSite:GeeksforGeeks.org";
-        String B = "NewSite:GeeksQuiz.com";
+        String A = "AGGTAB";
+        String B = "GXTXAYB";
         int m = A.length();
         int n = B.length();
         int[][] dp = new int[m + 1][n + 1];
@@ -23,6 +25,47 @@ public class PractiseXI {
         System.out.println(LongestCommonSubString(A, B, m, n, 0));
     }
 
+
+
+
+    public static void PrintLongestCommonSubSequence(String x, String y) {
+
+        int m = x.length();
+        int n = y.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i < m + 1; i++)
+            for (int j = 0; j < n + 1; j++)
+                if (i == 0 || j == 0) dp[i][j] = 0;
+
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (x.charAt(i - 1) == y.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        // Now Traverse the Matrix from bottom right and append the characters!
+        StringBuilder sb = new StringBuilder();
+
+        int i = m, j = n;
+
+        while (i > 0 && j > 0) {
+            if (x.charAt(i - 1) == y.charAt(j - 1)) {
+                sb.append(x.charAt(i - 1));
+                i--;
+                j--;
+                continue;
+            }
+            if (dp[i - 1][j] > dp[i][j]) {
+                i = i - 1;
+            } else j = j - 1;
+
+        }
+
+
+        System.out.println(sb.reverse());
+    }
 
 
     //https://www.geeksforgeeks.org/longest-common-substring-dp-29/
