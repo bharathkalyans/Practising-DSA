d63ad7789f8c3544e204461851afbc90cfb4470c
Bharath Kalyan S <sbharath35@gmail.com>
2021-10-10 13:04:17 +0530

    Some Important Question!

--- src/Miscellaenous.java
+++ src/Miscellaenous.java
@@ -6,14 +6,134 @@ import java.util.Map;
 public class Miscellaenous {
     public static void main(String[] args) {
 
-        int[] arr = {12, 3, 4, 1, 6, 9};
-        System.out.println(isTriplet(arr, 24));
+        int[] arr = {5, 4, 9, 8, 3, 6, 5, 4};
 
+        System.out.println(MergeArrayToMakePalindrome(arr, arr.length));
 
     }
 
 
+    public static int minOperations(int n) {
+        int[] arr = new int[n];
 
+        for (int i = 0; i < n; i++) arr[i] = (2 * i) + 1;
+
+        int min_operations = 0;
+        for (int i = 0; i < n / 2; i++) {
+            while (arr[i] != arr[n - i - 1]) {
+                arr[i] += 1;
+                arr[n - i - 1] -= 1;
+                min_operations++;
+            }
+        }
+        for (int x : arr) System.out.print(x + " ");
+        System.out.println();
+        return min_operations;
+    }
+
+    public static int MergeArrayToMakePalindrome(int[] arr, int n) {
+
+        if (arr == null || arr.length <= 1) return 0;
+        int i = 0, j = n - 1, answer = 0;
+        while (i < j) {
+            if (arr[i] == arr[j]) {
+                i++;
+                j--;
+            } else if (arr[i] > arr[j]) {
+                arr[j - 1] = arr[j - 1] + arr[j];
+                j--;
+                answer++;
+            } else {
+                arr[i + 1] = arr[i + 1] + arr[i];
+                i++;
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    //Very Good Question!!You have find sub array!
+    public static int minSwap(int[] arr, int n, int k) {
+
+        int good = 0;
+        for (int x : arr) if (x <= k) good++;
+
+        int bad = 0;
+        for (int i = 0; i < good; i++) if (arr[i] > k) bad++;
+
+        int answer = bad;
+
+        for (int i = 0, j = good; j < n; i++, j++) {
+            if (arr[i] > k) bad--;
+            if (arr[j] > k) bad++;
+            answer = Math.min(answer, bad);
+        }
+
+        return answer;
+    }
+
+    public int solve2(int[] arr, int k) {
+        int n = arr.length;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int odds = 0;
+            for (int j = i; j < n; j++) {
+                if (arr[j] % 2 != 0) odds++;
+            }
+            if (odds == k)
+                result++;
+        }
+        return result;
+    }
+
+
+    public static int smallestSubWithSum(int a[], int n, int x) {
+        int size = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += a[j];
+                if (sum > x)
+                    size = Math.min(size, j - i + 1);
+            }
+        }
+        return size;
+    }
+
+    public static long findMinDiff(int[] a, long n, long m) {
+        Arrays.sort(a);
+        long diff = Integer.MAX_VALUE;
+        int children = (int) m;
+        for (int i = 0; i + m < n; i++) {
+            diff = Math.min(a[i + children - 1] - a[i], diff);
+        }
+
+        return diff;
+    }
+
+    public static int trappingRainWater(int[] arr, int n) {
+        if (n == 1) return 0;
+        int result = 0;
+
+        for (int i = 1; i < n - 1; i++) {
+
+            int left_max = 0, right_max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                left_max = Math.max(left_max, arr[j]);
+            }
+
+            for (int k = i + 1; k < n; k++) {
+                right_max = Math.max(right_max, arr[k]);
+            }
+            int val = Math.min(left_max, right_max) - arr[i];
+            if (val > 0) result += val;
+
+        }
+
+        return Math.max(result, 0);
+    }
 
     public static boolean isTriplet(int[] arr, int K) {
         Arrays.sort(arr);
